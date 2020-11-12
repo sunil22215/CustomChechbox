02dc5cf8b22988273aa8916bf00400d8793d4cd1
sunil <sunilkhedkar22215@gmail.com>
2020-11-12 21:34:28 +0530

    mainactivity.java

--- app/src/main/java/com/example/customcheckbox/MainActivity.java
+++ app/src/main/java/com/example/customcheckbox/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.customcheckbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox cb1;
+    private CheckBox cb2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cb1=findViewById(R.id.checkbox3);
+        cb2=findViewById(R.id.checkbox4);
+        button=findViewById(R.id.btn1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder sb=new StringBuilder("");
+
+                if(cb1.isChecked()){
+                    String s1=cb1.getText().toString();
+                    sb.append(s1);
+                }
+                if(cb2.isChecked()){
+                    String s2=cb2.getText().toString();
+                    sb.append("\n"+s2);
+                }
+                if(sb!=null && !sb.toString().equals("")){
+                    Toast.makeText(getApplicationContext(),sb,Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Nothing selected",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
